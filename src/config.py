c14f5b0f70b0669f7e33f0b15da0f839cfeace46
Ashna A. <ashna0733@gmail.com>
2026-09-21 18:54:48 +0530

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,21 @@
+# src/config.py
+FEATURE_COLUMNS = [
+    'user_role', 'service_type', 'department', 'access_method', 'device_type',
+    'login_frequency', 'session_duration', 'time_since_last_login',
+    'location_distance_km', 'access_count_7d', 'failed_attempts_24h',
+    'session_requests', 'is_off_hours', 'location_mismatch', 'device_mismatch'
+]
+
+CATEGORICAL_FEATURES = ['user_role', 'service_type', 'department',
+                        'access_method', 'device_type']
+
+NUMERICAL_FEATURES = [
+    'login_frequency', 'session_duration', 'time_since_last_login',
+    'location_distance_km', 'access_count_7d', 'failed_attempts_24h',
+    'session_requests', 'is_off_hours', 'location_mismatch', 'device_mismatch'
+]
+
+TARGET_COLUMN = 'risk_level'
+RISK_LABELS = {0: 'ALLOW', 1: 'CHALLENGE_MFA', 2: 'DENY'}
+
+RANDOM_SEED = 42
