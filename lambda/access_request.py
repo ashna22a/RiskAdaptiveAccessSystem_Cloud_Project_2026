1623e84d9662bc4b66180a51a90fd65595199000
Krithikha Balaji <krithikha06@gmail.com>
2026-09-21 23:22:16 +0530

    Update access_request.py

--- lambda/access_request.py
+++ lambda/access_request.py
@@ -1,5 +1,14 @@
 def lambda_handler(event, context):
+    user_id = event.get("user_id")
+    service = event.get("service")
+    action = event.get("action")
+
     return {
         "statusCode": 200,
-        "body": "Access request Lambda is working"
+        "body": {
+            "message": "Access request received",
+            "user_id": user_id,
+            "service": service,
+            "action": action
+        }
     }
